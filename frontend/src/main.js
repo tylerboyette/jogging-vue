8142ac4ef00792f9ee9e668d0898bfac17b6836a
Adrian Telles <adriantellesweb@gmail.com>
2020-03-13 08:46:35 -0500

    user avatar uploading and email verification send update

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,23 +6,6 @@ import vuetify from './plugins/vuetify'
 import Toast from "vue-toastification";
 import "vue-toastification/dist/index.css";
 
-import firebase from 'firebase';
-import 'firebase/storage';
-var firebaseConfig = {
-  apiKey: "AIzaSyBbptOH7dBDj_S94G-5q38s6VwqRLrUZ38",
-  authDomain: "jogging-track-yuki.firebaseapp.com",
-  databaseURL: "https://jogging-track-yuki.firebaseio.com",
-  projectId: "jogging-track-yuki",
-  storageBucket: "jogging-track-yuki.appspot.com",
-  messagingSenderId: "857715225804",
-  appId: "1:857715225804:web:e981deb4aed9dfe24c7e69",
-  measurementId: "G-L1KVX9VHH0"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-const storage = firebase.storage();
-
 Vue.config.productionTip = false
 Vue.use(Toast)
 
